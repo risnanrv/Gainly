acf0dc4ea1fde0e0b6f690ab7ec69e419c8baf42
risnanrv <risnanrv.1877@gmail.com>
2026-04-10 20:12:33 +0530

    feat: configure service worker caching and workbox options in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,4 +8,10 @@ export default withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: false,
+  workboxOptions: {
+    disableDevLogs: true,
+    exclude: [/\.map$/, /^.*\/sw\.js$/],
+  }
 })(nextConfig);
